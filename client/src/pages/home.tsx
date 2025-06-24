aca987482e429916478a4f18049ca8aa97bf4f13
srysis <jashandenis@gmail.com>
2025-06-24 16:25:52 +0200

    implemented posts retrieval and display functionality

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,13 +1,36 @@
+import axios from '../api/axios'
+import { useState, useEffect } from 'react'
 
-function Home({isLoggedIn}) {
-	return(
-		<div>
-			<h1>Home page</h1>
-			<p>Welcome to home page</p>
-			{isLoggedIn && <p>You are currently logged in.</p>}
-			{!isLoggedIn && <p>You are not logged in.</p>}
-		</div>
-	)
+import Post from "../components/posts/Post"
+
+import "../style/home/posts.css"
+
+function Home() {
+	const [arePostsRetrieved, setArePostsRetrieved] = useState(false);
+	const [retrieved_posts, setRetrievedPosts] = useState([]);
+
+	useEffect(() => {
+		axios.get('/posts')
+		.then(response => {
+			setRetrievedPosts(response.data.posts);
+			setArePostsRetrieved(true);
+		})
+		.catch(error => console.log(error.response.data));
+	}, []);
+
+	if (arePostsRetrieved) {
+		return(
+			<div>
+				<h1>Home page</h1>
+				<p>Welcome to home page</p>
+				<section id="posts">
+					<h2>Recent posts</h2>
+					{retrieved_posts.map((post, index) => <Post key={index} content={post} />)}
+				</section>
+			</div>
+		)
+	}
+	
 }
 
 export default Home;
