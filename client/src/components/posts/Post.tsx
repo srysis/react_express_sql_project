8696af628a948340ba1d76ceeb9f632de93ee05d
srysis <jashandenis@gmail.com>
2025-06-26 15:28:06 +0200

    post's title is now clickable and will lead to it's page when clicked

--- client/src/components/posts/Post.tsx
+++ client/src/components/posts/Post.tsx
@@ -5,7 +5,7 @@ function Post({content}) {
 	return (
 		<article className="post">
 			<div className="post_title">
-				<p>{content.post_title}</p>
+				<Link to={`/post/${content.post_id}`}>{content.post_title}</Link>
 			</div>
 			<div className="post_author">
 				<Link to={`/user/${content.post_author}`}>{content.post_author_name}</Link>
