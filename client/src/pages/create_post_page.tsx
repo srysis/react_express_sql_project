02b51c1f4aaf4214da5a62ce60e7666d06532ddb
srysis <jashandenis@gmail.com>
2025-06-26 14:27:56 +0200

    wrapped 'create_post' request in a 'verify' request to make sure that user has not modified any localStorage data

--- client/src/pages/create_post_page.tsx
+++ client/src/pages/create_post_page.tsx
@@ -3,8 +3,9 @@ import { useNavigate } from 'react-router'
 import { useParams, Link } from 'react-router-dom'
 
 import axios from '../api/axios'
+import { setAuthorizationHeader } from '../tools/setHeaders'
 
-function CreatePostPage({isLoggedIn, USER_ID}) {
+function CreatePostPage({isLoggedIn, USER_ID, setLoggedIn, setHasAdminRights}) {
 	const navigate = useNavigate();
 
 	const { id } = useParams();
@@ -20,11 +21,37 @@ function CreatePostPage({isLoggedIn, USER_ID}) {
 	function onSubmitHandler(event) {
 		event.preventDefault();
 
-		axios.post(`/user/${USER_ID}/create_post`, { post: post_content })
+		/*
+			to make sure that user is allowed to post, make a 'verify' call to check
+			if user's client does not contain modified token or changed 'id'
+		*/
+
+		axios.get(`/verify/${window.localStorage.getItem('id')}`)
 		.then(response => {
-			console.log(response.data)
+			if (response.data.success) {
+
+				/* if user is successfully verified, create a post */
+				axios.post(`/user/${USER_ID}/create_post`, { post: post_content })
+				.then(response => {
+					console.log(response.data)
+				})
+				.catch(error => error.response.data)
+
+			}
 		})
-		.catch(error => error.response.data)
+		.catch(error => {
+			if (!error.response.data.success) {
+				setLoggedIn(false);
+				setHasAdminRights(false);
+
+				setAuthorizationHeader(null);
+
+				window.localStorage.removeItem('t');
+				window.localStorage.removeItem('id');
+
+				console.error(error.response.data.message);
+			}
+		})
 	}
 
 	function onChangeHandler(event) {
