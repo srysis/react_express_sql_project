9adf7b94cdc913cdddd8d633b378f5b8b31f88fc
srysis <jashandenis@gmail.com>
2025-07-01 12:34:23 +0200

    removed post's content paragraph; moved post's author container to the start

--- client/src/components/posts/Post.tsx
+++ client/src/components/posts/Post.tsx
@@ -4,6 +4,9 @@ import { Link } from 'react-router-dom'
 function Post({content}) {
 	return (
 		<article className="post">
+			<div className="post_author">
+				<Link to={`/user/${content.post_author}`}>{content.post_author_name}</Link>
+			</div>
 			<div className="link_container">
 				<div className="overlay">
 					<Link to={`/post/${content.post_id}`}></Link>
@@ -12,13 +15,7 @@ function Post({content}) {
 					<div className="post_title">
 						<h1>{content.post_title}</h1>
 					</div>
-					<div className="post_content">
-						<p>{content.post_content}</p>
-					</div>
 				</div>
-			</div>
-			<div className="post_author">
-				<Link to={`/user/${content.post_author}`}>{content.post_author_name}</Link>
 			</div>
 		</article>
 	)
