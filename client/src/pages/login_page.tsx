9f280612c7aff6107d3155f4b827e9e87d4802d9
srysis <jashandenis@gmail.com>
2025-07-05 15:46:06 +0200

    wrapped the whole HTML fragment in a 'section'; improved 'register' prompt; put text in 'labels' into 'span'

--- client/src/pages/login_page.tsx
+++ client/src/pages/login_page.tsx
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom'
 import axios from '../api/axios'
 import { setAuthorizationHeader } from '../tools/setHeaders'
 
+import "../style/auth_pages/login_page.css"
+
 function LoginPage({ isLoggedIn, setLoggedIn, setHasAdminRights }) {
 	const [user_credentials, setUserCredentials] = useState({});
 
@@ -54,23 +56,23 @@ function LoginPage({ isLoggedIn, setLoggedIn, setHasAdminRights }) {
 	return(
 		<>
 			{ !isLoggedIn && 
-				<div id="login">
+				<section id="login">
 					<h1>Login</h1>
 					<form onSubmit={onSubmitHandler}>
 						<div className="input_container">
-							<label htmlFor="username">Username:</label>
+							<label htmlFor="username"><span>Username</span></label>
 							<input type="text" id="username" name="username" autoComplete="off" onChange={onChangeHandler} />
 						</div>
 						<div className="input_container">
-							<label htmlFor="password">Password:</label>
-							<input type="password" name="password" onChange={onChangeHandler} />
+							<label htmlFor="password"><span>Password</span></label>
+							<input type="password" id="password" name="password" onChange={onChangeHandler} />
 						</div>
-						<div className="subimt_container">
+						<div className="submit_container">
 							<button>Login</button>
 						</div>
 					</form>
-					<Link to="/register">Register</Link>
-				</div>
+					<div id="register"><span>Don't have an account?</span><Link to="/register"> Register now.</Link></div>
+				</section>
 			}
 		</>
 	)
