9d24c09dcb0df3fd762f9cb3e63c166227930ce4
srysis <jashandenis@gmail.com>
2025-07-10 14:12:46 +0200

    '/post/:post_id' endpoint now handles retrieving posts made by deleted users and responds with appropriate data

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -19,17 +19,48 @@ router.get('/posts', (request, response) => {
 router.get('/post/:post_id', (request, response) => {
 	const requested_post_id = request.params.post_id;
 
-	const get_post_by_id_query = "SELECT `users_info`.`name` AS `post_author_name`, `user_posts`.`post_id`, `user_posts`.`post_title`, `user_posts`.`post_content`, `user_posts`.`post_date`, `user_posts`.`post_author` " +
-								 "FROM `users_info` INNER JOIN `user_posts` ON `users_info`.`user_id` = `user_posts`.`post_author` " +
-								 "WHERE `user_posts`.`post_id` = " + requested_post_id;
+	const select_post_query = "SELECT * FROM `user_posts` WHERE `post_id` = " + requested_post_id;
 
-	database.query(get_post_by_id_query, (error, data) => {
+	database.query(select_post_query, (error, data) => {
 		if (error) return response.json(error);
 
 		if (data.length) {
-			response.json({post: data[0]})
-		} else {
-			response.json({message: "Post does not exist or was deleted by it's author.", post: null})
+			const check_author_query = "SELECT `user_id`, `name` FROM `users_info` WHERE `user_id` = " + data[0].post_author;
+
+			database.query(check_author_query, (error, data) => {
+				if (error) return response.json(error);
+
+				if (!data.length) {
+					const get_post_by_id_query = "SELECT `users_info`.`name` AS `post_author_name`, `user_posts`.`post_id`, `user_posts`.`post_title`, `user_posts`.`post_content`, `user_posts`.`post_date`, `user_posts`.`post_author` " + 
+												 "FROM `user_posts` LEFT JOIN `users_info` ON `user_posts`.`post_author` = `users_info`.`user_id` " +
+								 				 "WHERE `user_posts`.`post_id` = " + requested_post_id;
+
+					database.query(get_post_by_id_query, (error, data) => {
+						if (error) return response.json(error);
+
+						if (data.length) {
+							response.json({post: data[0]})
+						} else {
+							response.json({message: "Post does not exist or was deleted by it's author.", post: null})
+						}
+					})
+
+				} else {
+					const get_post_by_id_query = "SELECT `users_info`.`name` AS `post_author_name`, `user_posts`.`post_id`, `user_posts`.`post_title`, `user_posts`.`post_content`, `user_posts`.`post_date`, `user_posts`.`post_author` " +
+								 "FROM `users_info` INNER JOIN `user_posts` ON `users_info`.`user_id` = `user_posts`.`post_author` " +
+								 "WHERE `user_posts`.`post_id` = " + requested_post_id;
+
+					database.query(get_post_by_id_query, (error, data) => {
+						if (error) return response.json(error);
+
+						if (data.length) {
+							response.json({post: data[0]})
+						} else {
+							response.json({message: "Post does not exist or was deleted by it's author.", post: null})
+						}
+					})
+				}
+			})
 		}
 	})
 });
