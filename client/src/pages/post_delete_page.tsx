0313ca9539f3ee1ac97beae4741e30d930c64507
srysis <jashandenis@gmail.com>
2025-07-15 15:45:27 +0200

    import a stylesheet file

--- client/src/pages/post_delete_page.tsx
+++ client/src/pages/post_delete_page.tsx
@@ -4,6 +4,8 @@ import { useParams, Link } from 'react-router-dom'
 
 import axios from '../api/axios'
 
+import "../style/post_page/post_delete_page.css"
+
 function PostDeletePage({USER_ID}) {
 	const REQUEST_HEADERS = {
 		'Content-Type': 'application/json'
