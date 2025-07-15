bdd68fb3611cdf6098f41fa0a071db2347eadc41
srysis <jashandenis@gmail.com>
2025-07-15 14:12:49 +0200

    added a route to 'edit post' page; minor formatting improvements

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,15 +7,21 @@ import { setAuthorizationHeader } from './tools/setHeaders'
 import ProtectedRoutes from './tools/ProtectedRoutes'
 
 import BaseLayout from './layout/Base'
+
 import Home from './pages/home'
+
 import RegistrationPage from './pages/registration_page'
 import LoginPage from './pages/login_page'
+
 import ProfilePage from './pages/profile_page'
 import ProfileOptionsPage from './pages/options_page'
 import ProfileEditPage from './pages/profile_edit_page'
 import ProfileDeletePage from './pages/profile_delete_page'
+
 import PostPage from './pages/post_page'
 import CreatePostPage from './pages/create_post_page'
+import PostEditPage from './pages/post_edit_page'
+
 import SearchPage from './pages/search_page'
 
 import "./style/shared.css"
@@ -98,6 +104,7 @@ function App() {
 						<Route path="/user/:id/edit" element={<ProfileEditPage isLoggedIn={isLoggedIn} />} />
 						<Route path="/user/:id/delete" element={<ProfileDeletePage isLoggedIn={isLoggedIn} setLoggedIn={setLoggedInStateWrapper} setHasAdminRights={setHasAdminRightsWrapper} />} />
 						<Route path="/user/:id/create_post" element={<CreatePostPage isLoggedIn={isLoggedIn} USER_ID={stored_user_ID} setLoggedIn={setLoggedInStateWrapper} setHasAdminRights={setHasAdminRightsWrapper} />} />
+						<Route path="/post/:post_id/edit" element={<PostEditPage />} />
 					</Route>
 
 				</Route>
