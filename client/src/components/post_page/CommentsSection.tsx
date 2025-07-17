0548542b76f729ed0b30808dbc453c4e4a0a2704
srysis <jashandenis@gmail.com>
2025-07-17 16:42:06 +0200

    added a check for 'comments' length before rendering anything; added a 'no comments' message if no comments exist for current post

--- client/src/components/post_page/CommentsSection.tsx
+++ client/src/components/post_page/CommentsSection.tsx
@@ -3,11 +3,20 @@ import Comment from "./Comment"
 import "../../style/post_page/comments_section.css"
 
 function CommentsSection({comments}) {
-	return(
-		<section id="comments">
-			{comments.map((comment, index) => <Comment key={index} comment={comment} />)}
-		</section>
-	)
+	if (comments.length) {
+		return(
+			<section id="comments">
+				{comments.map((comment, index) => <Comment key={index} comment={comment} />)}
+			</section>
+		)
+	} else {
+		return(
+			<section id="comments" className="empty">
+				<p>No comments yet.</p>
+			</section>
+		)
+	}
+	
 }
 
 export default CommentsSection;
