b0edf7e682016ce0bf06feb2c7490a7a25113734
srysis <jashandenis@gmail.com>
2025-07-17 14:15:13 +0200

    import 'CommentsSection' component; added states to keep track of current post's comments; wrapper 'section' in an empty root fragment; added a conditional rendering block for 'CommentsSection'

--- client/src/pages/posts/post_page.tsx
+++ client/src/pages/posts/post_page.tsx
@@ -3,6 +3,8 @@ import { useParams, Link } from 'react-router-dom'
 
 import axios from '../../api/axios'
 
+import CommentsSection from "../../components/post_page/CommentsSection"
+
 import dots_icon from "../../assets/v_dots-icon.png"
 
 import "../../style/post_page/post_page.css"
@@ -15,6 +17,9 @@ function PostPage({isLoggedIn}) {
 
 	const [post_ownership, setPostOwnership] = useState(false);
 
+	const [comments, setComments] = useState([]);
+	const [areCommentsRetrieved, setAreCommentsRetrieved] = useState(false);
+
 	useEffect(() => {
 		axios.get(`/post/${post_id}`)
 		.then(response => {
@@ -24,6 +29,14 @@ function PostPage({isLoggedIn}) {
 				setIsPostRetrieved(true);
 
 				setPostOwnership(response.data.post_ownership);
+
+				axios.get(`/post/${post_id}/comments`)
+				.then(response => {
+					setComments(response.data.comments);
+
+					setAreCommentsRetrieved(true);
+				})
+				.catch(error => console.error(error.response.data));
 			}
 
 		})
@@ -32,38 +45,41 @@ function PostPage({isLoggedIn}) {
 
 	if (isPostRetrieved) {
 		return(
-			<section id="post">
-				<div className="post_author">
-					<p>Author:</p>
-					{post_content.post_author_name !== null &&
-						<Link to={`/user/${post_content.post_author}`}>{post_content.post_author_name}</Link>
-					}
-					{post_content.post_author_name === null &&
-						<p style={{"fontStyle": "italic"}}>deleted</p>
-					}
-				</div>
-				<div className="wrapper">
-					<div className="post_content">
-						<div className="title">
-							<h1>{post_content.post_title}</h1>
-						</div>
-						<div className="content">
-							<p>{post_content.post_content}</p>
-						</div>
+			<>
+				<section id="post">
+					<div className="post_author">
+						<p>Author:</p>
+						{post_content.post_author_name !== null &&
+							<Link to={`/user/${post_content.post_author}`}>{post_content.post_author_name}</Link>
+						}
+						{post_content.post_author_name === null &&
+							<p style={{"fontStyle": "italic"}}>deleted</p>
+						}
 					</div>
-					{post_ownership && 
-						<div className="options_container">
-							<div className="icon_container" title="Post options" onClick={(event) => {document.querySelector("div.list_container").classList.toggle("active")}}>
-								<img src={dots_icon} />
+					<div className="wrapper">
+						<div className="post_content">
+							<div className="title">
+								<h1>{post_content.post_title}</h1>
 							</div>
-							<div className="list_container">
-								<Link to={`/post/${post_id}/edit`}>Edit</Link>
-								<Link to={`/post/${post_id}/delete`}>Delete</Link>
+							<div className="content">
+								<p>{post_content.post_content}</p>
 							</div>
 						</div>
-					}
-				</div>
-			</section>
+						{post_ownership && 
+							<div className="options_container">
+								<div className="icon_container" title="Post options" onClick={(event) => {document.querySelector("div.list_container").classList.toggle("active")}}>
+									<img src={dots_icon} />
+								</div>
+								<div className="list_container">
+									<Link to={`/post/${post_id}/edit`}>Edit</Link>
+									<Link to={`/post/${post_id}/delete`}>Delete</Link>
+								</div>
+							</div>
+						}
+					</div>
+				</section>
+				{areCommentsRetrieved && <CommentsSection comments={comments} />}
+			</>
 		)
 	}
 }
