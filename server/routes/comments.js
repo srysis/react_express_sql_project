98d54ae9a762f00b7098be51c91ebd0163700d0b
srysis <jashandenis@gmail.com>
2025-07-17 16:11:17 +0200

    fixed a typo in '/:post_id/comments/' that prevented proper selecting by post's ID

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -12,7 +12,7 @@ router.get('/:post_id/comments', (request, response) => {
 
 	const select_comments_of_post_query = "SELECT `comments`.*, `users_info`.`name` AS `comment_author_name` " + 
 										  "FROM `comments` INNER JOIN `users_info` ON `comments`.`comment_author` = `users_info`.`user_id` " + 
-										  "INNER JOIN `user_posts` ON `comments`.`post_id` = `user_posts`.`post_id` "
+										  "INNER JOIN `user_posts` ON `comments`.`post_id` = `user_posts`.`post_id` " +
 										  "WHERE `user_posts`.`post_id` = " + requested_post_id;
 
 	database.query(select_comments_of_post_query, (error, data) => {
