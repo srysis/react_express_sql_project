a20d21e57fe36516c82ce0eb3774b6345ad9c472
srysis <jashandenis@gmail.com>
2025-07-17 13:56:51 +0200

    modified SQL query inside '/:post_id/comments' request to also select both author's ID and his name

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -7,7 +7,10 @@ const router = express.Router();
 router.get('/:post_id/comments', (request, response) => {
 	const requested_post_id = request.params.post_id;
 
-	const select_comments_of_post_query = "SELECT * FROM `comments` WHERE `post_id` = " + requested_post_id;
+	const select_comments_of_post_query = "SELECT `comments`.*, `users_info`.`name` AS `comment_author_name` " + 
+										  "FROM `comments` INNER JOIN `users_info` ON `comments`.`comment_author` = `users_info`.`user_id` " + 
+										  "INNER JOIN `user_posts` ON `comments`.`post_id` = `user_posts`.`post_id` "
+										  "WHERE `user_posts`.`post_id` = " + requested_post_id;
 
 	database.query(select_comments_of_post_query, (error, data) => {
 		if (error) return response.json(error);
