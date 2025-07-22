01888dcac918ee61cb9cdee4bd05e87876dee506
srysis <jashandenis@gmail.com>
2025-07-22 16:38:31 +0200

    create component that is responsible for selecting and uploading images to the server

---
+++ client/src/components/profile_page/UploadProfilePictureForm.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react'
+
+import axios from '../../api/axios'
+
+function UploadProfilePictureForm({USER_ID}) {
+	const [image, setImage] = useState(null);
+
+
+	function onChangeHandler(event) {
+		if (event.target.files.length === 0) {
+			setImage(null);
+		} else {
+			const formData = new FormData();
+			formData.append('image-name', event.target.files[0], event.target.files[0].name);
+			setImage(formData);
+		}
+	}
+
+	async function onSubmitHandler(event) {
+		event.preventDefault();
+
+		try {
+			const response = await axios.post(`/user/${USER_ID}/upload_profile_picture`, image);
+
+			console.log(response);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	return(
+		<form encType="multipart/form-data" onSubmit={onSubmitHandler}>
+			<input type="file" accept=".jpg" name="profile_picture" onChange={onChangeHandler} />
+			<button>Submit</button>
+		</form>
+	)
+}
+
+export default UploadProfilePictureForm;
