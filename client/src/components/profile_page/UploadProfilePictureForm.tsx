4dd046dd5ff1ebe044010a1064bd160a809d0698
srysis <jashandenis@gmail.com>
2025-07-24 14:39:46 +0200

    implemented a 'image preview' feature

--- client/src/components/profile_page/UploadProfilePictureForm.tsx
+++ client/src/components/profile_page/UploadProfilePictureForm.tsx
@@ -2,17 +2,37 @@ import { useState, useEffect } from 'react'
 
 import axios from '../../api/axios'
 
-function UploadProfilePictureForm({USER_ID}) {
-	const [image, setImage] = useState(null);
+function UploadProfilePictureForm({USER_ID, defaultUserData}) {
+	const [image_data, setImageData] = useState(null);
+
+	const [selected_image, setSelectedImage] = useState(null);
+	const [preview_image, setPreviewImage] = useState(null);
+
+	useEffect(() => {
+		if (!selected_image) {
+			setPreviewImage(null);
+			return;
+		}
+
+		const imageObjectURL = URL.createObjectURL(selected_image);
+		setPreviewImage(imageObjectURL);
+
+		return () => URL.revokeObjectURL(imageObjectURL);
+	}, [selected_image])
 
 
 	function onChangeHandler(event) {
 		if (event.target.files.length === 0) {
-			setImage(null);
+			setImageData(null);
+			setSelectedImage(null);
+
+			return;
 		} else {
 			const formData = new FormData();
 			formData.append('image-name', event.target.files[0], event.target.files[0].name);
-			setImage(formData);
+			setImageData(formData);
+
+			setSelectedImage(event.target.files[0]);
 		}
 	}
 
@@ -20,7 +40,7 @@ function UploadProfilePictureForm({USER_ID}) {
 		event.preventDefault();
 
 		try {
-			const response = await axios.post(`/user/${USER_ID}/upload_profile_picture`, image);
+			const response = await axios.post(`/user/${USER_ID}/upload_profile_picture`, image_data);
 
 			console.log(response);
 		} catch (error) {
@@ -30,12 +50,16 @@ function UploadProfilePictureForm({USER_ID}) {
 
 	return(
 		<section id="upload_profile_picture">
+			<span className="title"><span>Profile picture</span></span>
+			<div id="profile_picture">
+				<img src={selected_image ? preview_image : `http://localhost:8081/${defaultUserData.profile_picture}`} />
+			</div>
 			<form encType="multipart/form-data" onSubmit={onSubmitHandler}>
 				<div className="input_container">
 					<input type="file" accept=".jpg" name="profile_picture" onChange={onChangeHandler} />
 				</div>
 				<div className="button_container">
-					<button disabled={!image}>Apply changes</button>
+					<button disabled={!image_data}>Apply changes</button>
 				</div>
 			</form>
 		</section>
