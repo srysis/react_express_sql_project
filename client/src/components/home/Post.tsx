ab3cd208eaf4874e70856c98e17f6b1011a56caa
srysis <jashandenis@gmail.com>
2025-07-31 14:19:40 +0200

    major layout changes; now displays author's profile picture and how long ago the post was made

--- client/src/components/home/Post.tsx
+++ client/src/components/home/Post.tsx
@@ -1,16 +1,26 @@
 import { Link } from 'react-router-dom'
 
-
 function Post({content}) {
 	return (
 		<article className="post">
-			<div className="post_author">
-				<Link to={`/user/${content.post_author}`}>{content.post_author_name}</Link>
-			</div>
 			<div className="link_container">
 				<div className="overlay">
 					<Link to={`/post/${content.post_id}`}></Link>
 				</div>
+				<div className="post_author">
+					<div className="image_container">
+						<Link to={`/user/${content.post_author}`}><img src={`http://localhost:8081/${content.post_author_avatar}`} /></Link>
+					</div>
+					<div className="name_and_date">
+						<div className="name">
+							<Link to={`/user/${content.post_author}`}>{content.post_author_name}</Link>
+						</div>
+						<div className="date">
+							<span>{content.date_difference}</span>
+						</div>
+					</div>
+					
+				</div>	
 				<div className="post_content">
 					<div className="post_title">
 						<h1>{content.post_title}</h1>
