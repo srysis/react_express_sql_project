fb7a65bb38a1db6d6c4aef22f37eafe15bdb650f
srysis <jashandenis@gmail.com>
2025-08-06 13:10:35 +0200

    use environment variables

--- server/database.js
+++ server/database.js
@@ -1,9 +1,12 @@
 const mysql = require('mysql2');
+const dot_env = require('dotenv');
+
+dot_env.config();
 
 const database = mysql.createConnection({
-	host: 'mysql-13377331-forumdb.c.aivencloud.com',
-	port: '19510',
-	user: 'avnadmin',
+	host: process.env.DATABASE_HOST,
+	port: process.env.DATABASE_PORT,
+	user: process.env.DATABASE_USERNAME,
 	password: process.env.DATABASE_PASSWORD,
 	database: 'forum_db'
 });
