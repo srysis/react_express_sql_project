ce1a0b7c51a73ccda1af0951ae345f9344f9225f
srysis <jashandenis@gmail.com>
2025-08-13 15:09:54 +0200

    expects 'post_type' as an element in 'content' object prop; removed commented line; destructurized 'content' object; 'div.post_title' is now 'div.title'; added a 'div.content' element which will contain post's content (a paragraph if post type is 'text' or a picture if 'image')

--- client/src/components/home/Post.tsx
+++ client/src/components/home/Post.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
 
 interface props {
-	content : {
+	content: {
 		date_difference: string,
 		post_author_name: string,
 		post_author_avatar: string,
@@ -9,35 +9,43 @@ interface props {
 		post_title: string,
 		post_content: string,
 		post_date: string,
-		post_author: number
+		post_author: number,
+		post_type: string
 	}
 }
 
 function Post({content}: props) {
+	const { date_difference, post_author_name, post_author_avatar, post_id, post_title, post_content, post_date, post_author, post_type } = content;
+
 	return (
 		<article className="post">
 			<div className="link_container">
 				<div className="overlay">
-					<Link to={`/post/${content.post_id}`}></Link>
+					<Link to={`/post/${post_id}`}></Link>
 				</div>
 				<div className="post_author">
 					<div className="image_container">
-						{/*<Link to={`/user/${content.post_author}`}><img src={`http://localhost:8081/${content.post_author_avatar}`} /></Link>*/}
-					<Link to={`/user/${content.post_author}`}><img src={`${import.meta.env.VITE_IMAGE_STORAGE}${content.post_author_avatar}`} /></Link>
+						<Link to={`/user/${post_author}`}><img src={`${import.meta.env.VITE_IMAGE_STORAGE}${post_author_avatar}`} /></Link>
 					</div>
 					<div className="name_and_date">
 						<div className="name">
-							<Link to={`/user/${content.post_author}`}>{content.post_author_name}</Link>
+							<Link to={`/user/${post_author}`}>{post_author_name}</Link>
 						</div>
 						<div className="date">
-							<span>{content.date_difference}</span>
+							<span>{date_difference}</span>
 						</div>
 					</div>
 					
 				</div>	
 				<div className="post_content">
-					<div className="post_title">
-						<h1>{content.post_title}</h1>
+					<div className="title">
+						<h1>{post_title}</h1>
+					</div>
+					<div className="content">
+						<div className={post_type === "image" ? "image_container" : "text_container"}>
+							{post_type === "text" && <p>{post_content}</p>}
+							{post_type === "image" && <img src={`${import.meta.env.VITE_POST_IMAGE_STORAGE}${post_content}`} />}
+						</div>
 					</div>
 				</div>
 			</div>
