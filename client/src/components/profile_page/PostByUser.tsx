0c994b04b2babb42f9b4b3873b9232c94ef78372
srysis <jashandenis@gmail.com>
2025-08-13 17:55:39 +0200

    depending on what type of a post it is, text or picture will be rendered

--- client/src/components/profile_page/PostByUser.tsx
+++ client/src/components/profile_page/PostByUser.tsx
@@ -8,24 +8,30 @@ interface props {
 		post_content: string,
 		post_date: string,
 		post_id: number,
-		post_title: string
+		post_title: string,
+		post_type: string
 	}
 }
 
 function PostByUser({content} : props) {
+	const { date_difference, post_content, post_date, post_id, post_title, post_type } = content;
+
 	return (
 		<article className="post">
 			<div className="link_container">
 				<div className="overlay">
-					<Link to={`/post/${content.post_id}`}></Link>
+					<Link to={`/post/${post_id}`}></Link>
 				</div>
 				<div className="post_content">
 					<div className="title_and_date">
-						<h3>{content.post_title}</h3>
-						<span title={content.post_date.split("T")[0]}>{content.date_difference}</span>
+						<h3>{post_title}</h3>
+						<span title={post_date.split("T")[0]}>{date_difference}</span>
 					</div>
 					<div className="content">
-						<p>{content.post_content.length > 1000 ? content.post_content.slice(0, 1000) + "..." : content.post_content}</p>
+						<div className={post_type === "image" ? "image_container" : "text_container"}>
+							{post_type === "text" && <p>{post_content}</p>}
+							{post_type === "image" && <img src={`${import.meta.env.VITE_POST_IMAGE_STORAGE}${post_content}`} />}
+						</div>
 					</div>
 				</div>
 			</div>
