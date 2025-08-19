fecfe59804839dc5b1be74aca91c698d8e7e3174
srysis <jashandenis@gmail.com>
2025-08-19 12:26:39 +0200

    text post content is now always being fully displayed; removed 'expandable content' functionality

--- client/src/pages/posts/post_page.tsx
+++ client/src/pages/posts/post_page.tsx
@@ -79,34 +79,6 @@ function PostPage({isLoggedIn}: props) {
 		.catch((error: any) => console.error(error.response.data))
 	}, [post_id])
 
-	useEffect(() => {
-		if (isPostRetrieved && post_content && post_content.post_type === "text") {
-			const post_container: any = document.querySelector("div.post_content");
-			const post_content_element: any = document.querySelector("div.post_content > div.content");
-
-			if (post_content_element.offsetHeight > post_container.offsetHeight) {
-				const initial_content: string = post_content.post_content;
-
-				const modified_content: string = initial_content.slice(0, 1800) + "...";
-
-				post_content_element.innerHTML = modified_content;
-				post_content_element.classList.add("expandable");
-				post_content_element.addEventListener("click", expandContent);
-			}
-		}
-	}, [isPostRetrieved]);
-
-	function expandContent(event: any) {
-		if (post_content) {
-			event.target.innerHTML = post_content.post_content;
-
-			const section_element : HTMLElement = document.querySelector('section#post') as HTMLElement;
-			if (section_element) section_element.style.maxHeight = 'none';
-			
-			event.target.classList.remove("expandable");
-		}
-	}
-
 	if (isPostRetrieved && post_content !== null) {
 		return(
 			<>
