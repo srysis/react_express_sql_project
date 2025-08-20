a9d8d42b42bd2ec7252bcb982121af819350805c
srysis <jashandenis@gmail.com>
2025-08-20 16:50:09 +0200

    add a new '/post/:post_id/get_rating' route that gets post's likes and dislikes amount

--- server/routes/rating.js
+++ server/routes/rating.js
@@ -8,6 +8,30 @@ const checkPostOwner = require('../middlewares/checkPostOwner.js');
 
 const router = express.Router();
 
+router.get('/post/:post_id/get_rating', (request, response) => {
+	const post_id = request.params.post_id;
+
+	const select_post_query = "SELECT * FROM `user_posts` WHERE `post_id` = " + post_id;
+
+	database.query(select_post_query, (error, data) => {
+		if (error) return response.status(500).json({success: false, message: "Something went wrong."});
+
+		if (!data.length) {
+			return response.json({success: false, message: "Post does not exist."})
+		}
+
+		const get_rating_query = "SELECT CAST(SUM(`user_posts_ratings`.`like`) AS SIGNED) AS likes_amount, CAST(SUM(`user_posts_ratings`.`dislike`) AS SIGNED) AS dislikes_amount " + 
+								 "FROM `users_info` INNER JOIN `user_posts` ON `users_info`.`user_id` = `user_posts`.`post_author` INNER JOIN `user_posts_ratings` ON `user_posts`.`post_id` = `user_posts_ratings`.`post_id` " +
+								 "WHERE `user_posts`.`post_id` = '" + post_id + "' GROUP BY `user_posts`.`post_id`";
+
+		database.query(get_rating_query, (error, data) => {
+			if (error) return response.status(500).json({success: false, message: "Something went wrong."});
+
+			response.json({success: true, likes: data[0].likes_amount, dislikes: data[0].dislikes_amount});
+		})
+	})
+})
+
 router.post('/post/:post_id/:id/like', [authenticator, checkPostOwner], (request, response) => {
 	if (response.locals.post_ownership) {
 		return response.json({success: false, message: "Post owners cannot affect their own posts' ratings"});
