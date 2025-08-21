cd42838c91eabaaaecdee786a5feb22a40538b48
srysis <jashandenis@gmail.com>
2025-08-21 14:09:16 +0200

    component now checks if rating has been set by current user on component init if user is logged in; accept and pass component along to child components

--- client/src/components/post_page/Rating.tsx
+++ client/src/components/post_page/Rating.tsx
@@ -6,14 +6,18 @@ import LikePostButton from "./LikePostButton"
 import DislikePostButton from "./DislikePostButton"
 
 interface props {
-	post_id: number
+	post_id: number,
+	isLoggedIn: boolean
 }
 
-function Rating({post_id}: props) {
+function Rating({post_id, isLoggedIn}: props) {
 	const [update_count, forceUpdate] = useReducer(x => x + 1, 0);
 
 	const [likes, setLikes] = useState<number>(0);
 	const [dislikes, setDislikes] = useState<number>(0);
+
+	const [isLiked, setIsLiked] = useState<boolean | null>(null);
+	const [isDisliked, setIsDisliked] = useState<boolean | null>(null);
 
 	useEffect(() => {
 		axios.get(`post/${post_id}/get_rating`)
@@ -21,6 +25,14 @@ function Rating({post_id}: props) {
 			if (response.data.success) {
 				setLikes(response.data.likes);
 				setDislikes(response.data.dislikes);
+
+				if (isLoggedIn) {
+					axios.get(`post/${post_id}/${window.localStorage.getItem('id')}/check_if_rated`)
+					.then((response: any) => {
+						setIsLiked(response.data.liked);
+						setIsDisliked(response.data.disliked);
+					})
+				}
 			}
 		})
 		.catch((error: any) => { console.error(error); })
@@ -28,8 +40,8 @@ function Rating({post_id}: props) {
 
 	return (
 		<>
-			<p>{likes}<LikePostButton post_id={post_id} forceUpdate={forceUpdate}/></p>
-			<p>{dislikes}<DislikePostButton post_id={post_id} forceUpdate={forceUpdate}/></p>
+			<p>{likes}<LikePostButton post_id={post_id} forceUpdate={forceUpdate} isLiked={isLiked} isLoggedIn={isLoggedIn}/></p>
+			<p>{dislikes}<DislikePostButton post_id={post_id} forceUpdate={forceUpdate} isDisliked={isDisliked} isLoggedIn={isLoggedIn}/></p>
 		</>
 	)
 }
