7a1c906b17005fd378c44dc457ab38510e272b71
srysis <jashandenis@gmail.com>
2025-08-21 14:10:14 +0200

    user will be redirected to 'login' page, if they attempt to set rating without being logged in

--- client/src/components/post_page/DislikePostButton.tsx
+++ client/src/components/post_page/DislikePostButton.tsx
@@ -1,23 +1,30 @@
+import { useNavigate } from 'react-router'
+
 import axios from '../../api/axios'
 
 interface props {
 	post_id: number,
-	forceUpdate: Function
+	forceUpdate: Function,
+	isDisliked: boolean,
+	isLoggedIn: boolean
 }
 
-function DislikePostButton({post_id, forceUpdate}: props) {
+function DislikePostButton({post_id, forceUpdate, isDisliked, isLoggedIn}: props) {
+	const navigate = useNavigate();
 
 	async function onClickHandler() {
-		try {
-			const response: any = await axios.post(`/post/${post_id}/${window.localStorage.getItem('id')}/dislike`);
+		if (isLoggedIn) {
+			try {
+				const response: any = await axios.post(`/post/${post_id}/${window.localStorage.getItem('id')}/dislike`);
 
-			console.log(response.data);
-
-			if (response.data.success) {
-				forceUpdate();
+				if (response.data.success) {
+					forceUpdate();
+				}
+			} catch (error: any) {
+				console.error(error.response.data.message);
 			}
-		} catch (error: any) {
-			console.error(error.response.data.message);
+		} else {
+			navigate('/login');
 		}
 	}
 
--- client/src/components/post_page/LikePostButton.tsx
+++ client/src/components/post_page/LikePostButton.tsx
@@ -1,23 +1,30 @@
+import { useNavigate } from 'react-router'
+
 import axios from '../../api/axios'
 
 interface props {
 	post_id: number,
-	forceUpdate: Function
+	forceUpdate: Function,
+	isLiked: boolean,
+	isLoggedIn: boolean
 }
 
-function LikePostButton({post_id, forceUpdate}: props) {
+function LikePostButton({post_id, forceUpdate, isLiked, isLoggedIn}: props) {
+	const navigate = useNavigate();
 
 	async function onClickHandler() {
-		try {
-			const response: any = await axios.post(`/post/${post_id}/${window.localStorage.getItem('id')}/like`);
+		if (isLoggedIn) {
+			try {
+				const response: any = await axios.post(`/post/${post_id}/${window.localStorage.getItem('id')}/like`);
 
-			console.log(response.data);
-
-			if (response.data.success) {
-				forceUpdate();
+				if (response.data.success) {
+					forceUpdate();
+				}
+			} catch (error: any) {
+				console.error(error.response.data.message);
 			}
-		} catch (error: any) {
-			console.error(error.response.data.message);
+		} else {
+			navigate('/login');
 		}
 	}
 
