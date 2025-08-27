a7251d7c80178ff7d91d93d49dd476efae32d1b4
srysis <jashandenis@gmail.com>
2025-08-27 23:27:22 +0200

    deleted 'setHeaders' module that is no longer neccessary

--- client/src/tools/setHeaders.ts
+++
@@ -1,9 +0,0 @@
-import axios from '../api/axios'
-
-export function setAuthorizationHeader(token: string | null) {
-	if (token) {
-		axios.defaults.headers.common['Authorization'] = token;
-	} else {
-		delete axios.defaults.headers.common['Authorization'];
-	}
-}
