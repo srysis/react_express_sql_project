c940e8546feab80755ab97a62383f8beb448874c
srysis <jashandenis@gmail.com>
2025-09-07 12:34:10 +0200

    import new stylesheet file for smaller screens; implemented 'discard changes' button

--- client/src/pages/posts/post_edit_page.tsx
+++ client/src/pages/posts/post_edit_page.tsx
@@ -8,6 +8,8 @@ import half_circle from "../../assets/half-circle.png"
 
 import "../../style/post_page/post_edit_page.css"
 
+import "../../style/mobile/post_page/post_edit_page.css"
+
 type Post = {
 	post_author_name: string,
 	post_author_avatar: string,
@@ -24,6 +26,8 @@ function PostEditPage() {
 	const navigate = useNavigate();
 
 	const { post_id } = useParams();
+
+	const [old_post_content, setOldPostContent] = useState<string>("");
 
 	const [post_data, setPostData] = useState<Post | null>(null);
 	const [isPostRetrieved, setIsPostRetrieved] = useState<boolean>(false);
@@ -43,11 +47,20 @@ function PostEditPage() {
 
 				setPostData(response.data.post);
 				setIsPostRetrieved(true);
+
+				setOldPostContent(response.data.post.post_content);
 			}
 
 		})
 		.catch((error: any) => console.error(error.response.data))
 	}, [post_id]);
+
+	function discardChanges() {
+		setNewPostContent("");
+		setWasPostChanged(false);
+
+		(document.querySelector("textarea[id='post_content']") as HTMLInputElement).value = old_post_content;
+	}
 
 	function onChangeHandler(event: any) {
 		setNewPostContent(event.target.value);
@@ -58,10 +71,16 @@ function PostEditPage() {
 	async function onSubmitHandler(event: any) {
 		event.preventDefault();
 
-		const button = document.querySelector("div.button_container > button");
-		if (button) { 
-			button.setAttribute("disabled", true.toString());
-			button.innerHTML = `<span class="loading_spinner"><img src=${half_circle} /></span>`;
+		const submit_button = document.querySelector("div.button_container > button[type='submit']");
+		const discard_button = document.querySelector("div.button_container > button[type='button']");
+
+		if (submit_button) { 
+			submit_button.setAttribute("disabled", true.toString());
+			submit_button.innerHTML = `<span class="loading_spinner"><img src=${half_circle} /></span>`;
+		}
+
+		if (discard_button) {
+			discard_button.setAttribute("disabled", true.toString());
 		}
 
 		try {
@@ -88,7 +107,8 @@ function PostEditPage() {
 						<textarea id="post_content" rows={4} cols={50} placeholder="Share your opinions..." defaultValue={post_data.post_content} onChange={onChangeHandler}></textarea>
 					</div>
 					<div className="button_container">
-						<button disabled={!wasPostChanged}>Apply changes</button>
+						<button type="button" disabled={!wasPostChanged} onClick={discardChanges}>Discard changes</button>
+						<button type="submit" disabled={!wasPostChanged}>Apply changes</button>
 					</div>
 				</form>
 			</section>
