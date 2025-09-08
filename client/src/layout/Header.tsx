5145b2075733abb05604f22186db7061e8894deb
srysis <jashandenis@gmail.com>
2025-09-08 14:05:19 +0200

    'nav' block will now be rendered on desktop only; render 'toggle aside' button on mobile; import new stylesheet file for smaller screens

--- client/src/layout/Header.tsx
+++ client/src/layout/Header.tsx
@@ -1,38 +1,50 @@
 import { Link } from 'react-router-dom'
 
+import bars_solid from "../assets/bars-solid.png"
+
 import "../style/layout/header.css"
+import "../style/mobile/layout/header.css"
 
 interface props {
+	DEVICE_TYPE: string,
 	isLoggedIn: boolean,
-	logOff: Function
+	logOff: Function,
+	setAsideVisibility: Function
 }
 
-function Header({isLoggedIn, logOff}: props) {
+function Header({DEVICE_TYPE, isLoggedIn, logOff, setAsideVisibility}: props) {
 	const stored_user_id : string | null = window.localStorage.getItem('id');
 
 	return(
 		<header>
+			{ DEVICE_TYPE === "mobile" && 
+				<div id="toggle_aside">
+					<button type="button" onTouchStart={() => {setAsideVisibility(true)}}><img src={bars_solid} /></button>
+				</div>
+			}
 			<div id="logo_container">
-				<h1><Link to="/">Logo</Link></h1>
+				<h1><Link to="/">Forum</Link></h1>
 			</div>
-			<nav>
-				<Link to="/search">Search</Link>
-				
-				{!isLoggedIn && 
-					<> 
-						<Link id="log_in_button" to="/login">Log In</Link>
-					</> 
-				}
+			{ DEVICE_TYPE === "desktop" &&
+				<nav>
+					<Link to="/search">Search</Link>
+					
+					{!isLoggedIn && 
+						<> 
+							<Link id="log_in_button" to="/login">Log In</Link>
+						</> 
+					}
 
-				{isLoggedIn && 
-					<> 
-						<Link to={`/user/${stored_user_id}`}>Profile</Link>
-						<Link to={`/user/${stored_user_id}/create_post`}>Create a post</Link>
-						<button id="log_off_button" onClick={() => { logOff(); }}>Log Off</button>
-					</>
-				}
+					{isLoggedIn && 
+						<> 
+							<Link to={`/user/${stored_user_id}`}>Profile</Link>
+							<Link to={`/user/${stored_user_id}/create_post`}>Create a post</Link>
+							<button id="log_off_button" onClick={() => { logOff(); }}>Log Off</button>
+						</>
+					}
 
-			</nav>
+				</nav>
+			}
 		</header>
 	)
 }
