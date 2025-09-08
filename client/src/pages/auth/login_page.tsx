07ba650cfc9d9bb9b8109b15f10639eaa40aa420
srysis <jashandenis@gmail.com>
2025-09-08 13:48:55 +0200

    changed 'document.querySelector' parameter in 'onSubmitHandler'

--- client/src/pages/auth/login_page.tsx
+++ client/src/pages/auth/login_page.tsx
@@ -53,7 +53,7 @@ function LoginPage({ isLoggedIn, logIn }: props) {
 	async function onSubmitHandler(event: any) {
 		event.preventDefault();
 
-		const button = document.querySelector("button");
+		const button = document.querySelector("div.submit_container > button");
 		if (button) { 
 			button.setAttribute("disabled", true.toString());
 			button.innerHTML = `<span class="loading_spinner"><img src=${half_circle} /></span>`;
