cff64b621e0c9eaf4c1f13d3858268c298603e2e
srysis <jashandenis@gmail.com>
2025-09-08 14:04:00 +0200

    import an 'Aside' component; add a state to control 'aside' visibility; render 'Aside' block on mobile devices

--- client/src/layout/Base.tsx
+++ client/src/layout/Base.tsx
@@ -1,10 +1,13 @@
+import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 
 import Header from "./Header"
+import Aside from './Aside'
 
 import Notification from "../components/NotificationMessage"
 
 interface props {
+	DEVICE_TYPE: string,
 	isLoggedIn: boolean,
 	logOff: Function,
 	notification_visible: boolean,
@@ -12,10 +15,17 @@ interface props {
 	notification_type: string
 }
 
-function Base({isLoggedIn, logOff, notification_visible, notification_message, notification_type}: props) {
+function Base({DEVICE_TYPE, isLoggedIn, logOff, notification_visible, notification_message, notification_type}: props) {
+	const [aside_visibility, setAsideVisibility] = useState<boolean>(false);
+
+	function setAsideVisibilityWrapper(value: boolean) {
+		setAsideVisibility(value);
+	}
+
 	return(
 		<>
-			<Header isLoggedIn={isLoggedIn} logOff={logOff} />
+			<Header DEVICE_TYPE={DEVICE_TYPE} isLoggedIn={isLoggedIn} logOff={logOff} setAsideVisibility={setAsideVisibilityWrapper} />
+			{ DEVICE_TYPE === "mobile" && <Aside isLoggedIn={isLoggedIn} logOff={logOff} visibility={aside_visibility} setAsideVisibility={setAsideVisibilityWrapper} /> }
 			{notification_visible && 
 				<Notification message={notification_message} type={notification_type} />
 			}
