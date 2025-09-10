98a10b2b3d7541b3e5f76e69ea8c278a226c2d93
srysis <jashandenis@gmail.com>
2025-09-10 17:32:03 +0200

    added an overlay element that triggers the notification removal upon clicking on it

--- client/src/components/NotificationMessage.tsx
+++ client/src/components/NotificationMessage.tsx
@@ -6,12 +6,15 @@ import "../style/mobile/notification.css"
 
 interface props {
 	message: string,
-	type: string
+	type: string,
+	setNotificationMessage: Function, 
+	setNotificationType: Function
 }
 
-function NotificationMessage({message, type}: props) {
+function NotificationMessage({message, type, setNotificationMessage, setNotificationType}: props) {
 	return (
 		<div id="notification" className={type === "success" ? "success" : "error"}>
+			<div className="overlay" onClick={() => { setNotificationMessage(""); setNotificationType("") }}></div>
 			<div className="icon_container">
 				<img src={type === "success" ? acknowledge_icon : error_icon} />
 			</div>
