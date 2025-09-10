744db057347add423ded1355c1752e6f2e0f8d28
srysis <jashandenis@gmail.com>
2025-09-10 17:30:19 +0200

    pass notification related functions to 'Notification' component

--- client/src/layout/Base.tsx
+++ client/src/layout/Base.tsx
@@ -12,10 +12,12 @@ interface props {
 	logOut: Function,
 	notification_visible: boolean,
 	notification_message: string,
-	notification_type: string
+	notification_type: string,
+	setNotificationMessage: Function,
+	setNotificationType: Function
 }
 
-function Base({DEVICE_TYPE, isLoggedIn, logOut, notification_visible, notification_message, notification_type}: props) {
+function Base({DEVICE_TYPE, isLoggedIn, logOut, notification_visible, notification_message, notification_type, setNotificationMessage, setNotificationType}: props) {
 	const [aside_visibility, setAsideVisibility] = useState<boolean>(false);
 
 	function setAsideVisibilityWrapper(value: boolean) {
@@ -27,7 +29,7 @@ function Base({DEVICE_TYPE, isLoggedIn, logOut, notification_visible, notificati
 			<Header DEVICE_TYPE={DEVICE_TYPE} isLoggedIn={isLoggedIn} logOut={logOut} setAsideVisibility={setAsideVisibilityWrapper} />
 			{ DEVICE_TYPE === "mobile" && <Aside isLoggedIn={isLoggedIn} logOut={logOut} visibility={aside_visibility} setAsideVisibility={setAsideVisibilityWrapper} /> }
 			{notification_visible && 
-				<Notification message={notification_message} type={notification_type} />
+				<Notification message={notification_message} type={notification_type} setNotificationMessage={setNotificationMessage} setNotificationType={setNotificationType} />
 			}
 			<main>
 				<Outlet />
