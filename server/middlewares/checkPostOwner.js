04265fa6108bc0ab19bb9f4fcc1aefb8fbd34c41
srysis <jashandenis@gmail.com>
2025-09-11 12:38:46 +0200

    added an 'authorization' header check before attempting to do anything

--- server/middlewares/checkPostOwner.js
+++ server/middlewares/checkPostOwner.js
@@ -4,35 +4,40 @@ const database = require('../database.js');
 
 function checkPostOwner(request, response, next) {	
 	const token = request.headers['authorization'];
-	const requested_post_id = request.params.post_id;
 
-	const select_post_query = "SELECT * FROM `user_posts` WHERE `post_id` = " + requested_post_id;
+	if (token) {
+		const requested_post_id = request.params.post_id;
 
-	database.query(select_post_query, (error, data) => {
-		if (error) return response.json(error);
+		const select_post_query = "SELECT * FROM `user_posts` WHERE `post_id` = " + requested_post_id;
 
-		if (data.length) {
-			try {
-				const decoded_token = jwt.verify(token, access_key);
+		database.query(select_post_query, (error, data) => {
+			if (error) return response.json(error);
 
-				if (decoded_token.id == data[0].post_author) {
-					response.locals.post_ownership = true;
-				} else {
-					response.locals.post_ownership = false;
+			if (data.length) {
+				try {
+					const decoded_token = jwt.verify(token, access_key);
+
+					if (decoded_token.id == data[0].post_author) {
+						response.locals.post_ownership = true;
+					} else {
+						response.locals.post_ownership = false;
+					}
+				} catch (error) {
+					if (error.name == "TokenExpiredError") {
+						return response.status(401).json({success: false, message: "Passed token has been expired.", refreshable: true});
+					} else {
+						response.locals.post_ownership = false;
+					}
 				}
-			} catch (error) {
-				if (error.name == "TokenExpiredError") {
-					return response.status(401).json({success: false, message: "Passed token has been expired.", refreshable: true});
-				} else {
-					response.locals.post_ownership = false;
-				}
+			} else {
+				response.locals.post_ownership = false;
 			}
-		} else {
-			response.locals.post_ownership = false;
-		}
 
-		next();
-	});
+			next();
+		});
+	} else {
+		return response.status(401).json({success: false, message: "No authorization header passed."});
+	}
 }
 
 module.exports = checkPostOwner;
