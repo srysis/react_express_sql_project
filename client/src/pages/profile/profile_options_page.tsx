2964515ca7385ca1ef3ca5209963adf57acbd2e0
srysis <jashandenis@gmail.com>
2025-09-11 11:44:15 +0200

    added description text to every option

--- client/src/pages/profile/profile_options_page.tsx
+++ client/src/pages/profile/profile_options_page.tsx
@@ -23,10 +23,12 @@ function ProfileOptionsPage({isAdmin}: props) {
 		<section id="options">
 			<div className="edit">
 				<div><Link to={`/user/${id}/edit`}>Edit profile</Link></div>
+				<p>Change your profile's name, description or avatar</p>
 			</div>
 			{!isAdmin && 
 				<div className="delete">
 					<div><Link to={`/user/${id}/delete`}>Delete profile</Link></div>
+					<p>Delete your profile and make it inaccessible</p>
 				</div>
 			}
 		</section>
