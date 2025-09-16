f20aa3a3bc3a8cbc2990cd0b3f899fcd64edba66
srysis <jashandenis@gmail.com>
2025-09-16 16:11:44 +0200

    comment field will now expand/shrink depending on it's scroll height

--- client/src/components/post_page/AddCommentField.tsx
+++ client/src/components/post_page/AddCommentField.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import axios from '../../api/axios'
 
@@ -12,12 +12,48 @@ interface props {
 }
 
 function AddCommentField({post_id}: props) {
+	const [initial_comment_field_height, setInitialCommentFieldHeight] = useState<number| null>(null);
+	const [current_comment_field_height, setCurrentCommentFieldHeight] = useState<number| null>(null);
+
 	const [comment, setComment] = useState<string>("");
 
 	const [action_in_progress, setActionInProgress] = useState<boolean>(false);
 
+	useEffect(() => {
+		const comment_field: HTMLElement | null = document.querySelector("textarea#comment_field");
+
+		if (comment_field != null) {
+			setInitialCommentFieldHeight(comment_field.clientHeight);
+			setCurrentCommentFieldHeight(comment_field.clientHeight);
+		}
+	}, [])
+
+	function isOverflowing(element: HTMLElement) {
+		return element.scrollHeight > element.clientHeight;
+	}
+
+	function isNotOverflowing(element: HTMLElement) {
+		return element.scrollHeight <= element.clientHeight;
+	}
+
 	function onChangeHandler(event: any) {
 		setComment(event.target.value);
+
+		if (isOverflowing(event.target)) {
+			event.target.style.height = `${event.target.scrollHeight}px`;
+			setCurrentCommentFieldHeight(event.target.scrollHeight);
+		} else if (isNotOverflowing(event.target)) {
+			event.target.style.height = "auto";
+			setCurrentCommentFieldHeight(initial_comment_field_height);
+		}
+	}
+
+	function onBlurHandler(event: any) {
+		event.target.style.height = `${initial_comment_field_height}px`;
+	}
+
+	function onFocusHandler(event: any) {
+		event.target.style.height = `${current_comment_field_height}px`;
 	}
 
 	async function onSubmitHandler(event: any) {
@@ -47,7 +83,7 @@ function AddCommentField({post_id}: props) {
 		<section id="add_comment">
 			<form onSubmit={onSubmitHandler}>
 				<div className="input_container" style={!comment ? {"marginBottom": "0"} : undefined}>
-					<textarea id="description" placeholder="Comment on this post" onChange={onChangeHandler}></textarea>
+					<textarea id="comment_field" maxLength={2048} placeholder="Comment on this post" onChange={onChangeHandler} onFocus={onFocusHandler} onBlur={onBlurHandler}></textarea>
 				</div>
 				<div className="button_container" style={!comment ? {"display": "none"} : undefined}>
 					<button disabled={!comment}>{ action_in_progress ? <LoadingSpinnerInline /> : "Comment" }</button>
